dc3206ad48028d863aeecadd75a665ab2f89931e
yuseungmin <dbtmdals7213@gmail.com>
2026-07-22 12:46:22 +0900

    2026 07 22 4교시

--- 13메소드오버로딩/src/Main.java
+++ 13메소드오버로딩/src/Main.java
@@ -10,25 +10,25 @@ class Calculator {
 	
 	//1. 두 정수를 매개변수로 전달받아 합을 구해 반환하는 기능의 add 메소드 만들기
 	public int add(int num1, int num2) {
-		
+					//	 5 ,	  10
 		return num1 + num2;
 	}
 	
 	//2. 세 개의 정수를 매개변수로 전달받아 합을 구해 반환하는 기능의 add 메소드 만들기
 	public int add(int num1, int num2, int num3) {
-		
+					//	 5 ,	  10 ,		15
 		return num1 + num2 + num3;
 	}
 	
 	//3. 두 실수를 매개변수로 전달받아 두 실수의 합을 구해 반환하는 기능의 add 메소드 만들기
 	public double add(double num1, double num2) {
-		
+						//	 5.5 ,		  10.5
 		return num1 + num2;
 	}
 	
 	//4. 하나의 정수와 하나의 실수를 순서대로 각각 매개변수로 전달받아 합을 구해 반환하는 기능의 add 메소드 만들기
 	public double add(int num1, double num2) {
-		
+						//	5 ,		   10.5
 		return num1 + num2;
 	}
 	
@@ -42,17 +42,23 @@ public class Main {
 		Calculator calculator = new Calculator();
 		
 		/* 정수 5와 정수 10의 합을 구해 이 자리에 15 출력 */
-		System.out.println("정수 5와 정수 10의 합: " + calculator.add(5, 10));
+		int result = calculator.add(5, 10); // <--- public int add(int num1, int num2){} 메소드 호출
+		System.out.println(result);
+						// 15
 		
 		/* 정수 5, 정수 10, 정수 15의 합을 구해 이 자리에 30 출력 */
-		System.out.println("정수 5, 정수 10, 정수 15의 합: " + calculator.add(5, 10, 15));
+		result = calculator.add(5, 10, 15); // <--- public int add(int num1, int num2, int num3){} 메소드 호출
+		System.out.println(result);
+						// 30
 		
 		/* 실수 5.5, 실수 10.5의 합을 구해 이 자리에 16.0 출력 */
-		System.out.println("실수 5.5, 실수 10.5의 합: " + calculator.add(5.5, 10.5));
+		double sum = calculator.add(5.5, 10.5); // <--- public double add(double num1, double num2){} 메소드 호출
+		System.out.println(sum);
+						// 16.0
 		
 		/* 정수 5, 실수 10.5의 합을 구해 이 자리에 15.5 출력 */
-		System.out.println("정수 5, 실수 10.5의 합: " + calculator.add(5, 10.5));
-		
+		System.out.println(calculator.add(5, 10.5)); // <--- public double add(int num1, double num2){} 메소드 호출
+						// 15.5
 	}// --- main 메소드
 
 }// --- Main 클래스
