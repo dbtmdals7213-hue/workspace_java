3b4b14680ed1d8594271400ee5b92e4e977870b9
yuseungmin <dbtmdals7213@gmail.com>
2026-08-05 13:55:37 +0900

    2026 08 05 4교시

--- 24자바_기본모듈_클래스들/src/리플렉션/GetClassExample.java
+++ 24자바_기본모듈_클래스들/src/리플렉션/GetClassExample.java
@@ -1,12 +1,52 @@
 package 리플렉션;
 
+// 메타 정보를 읽어볼 대상 클래스. 멤버가 하나도 없는 빈 클래스이다.
+// (멤버가 없어도 "리플렉션.Car 라는 클래스가 존재한다" 는 메타 정보는 존재한다.)
+class Car {
+	
+}
+
 public class GetClassExample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
+		//[1] 클래스 명을 가지고 Class 객체를 얻는 방법
 		
+		//방법1.	Class 참조변수 = 클래스명.class;
+		// Class claszz = Car.class;
+		// 만들어져 있는 class Car {} 의 메타 정보가 저장된 Class 객체 얻기
 		
+		//방법2.	Class 참조변수 = Class.forName("패키지.클래스명");
+		// Class claszz = Class.forName("리플렉션.Car");
+		// 만들어져 있는 class Car {} 의 메타 정보가 저장된 Class 객체 얻기
+
 		
+		//[2] 객체로부터 Class 객체를 얻는 방법
+		
+		//방법3.	Class 참조변수 = 생성된_객체_참조변수명.getClass();
+		//순서1.	먼저 class Car 의 객체를 생성한다.
+		Car car = new Car();
+		
+		//순서2.	getClass(): 이 객체를 만들 때 사용된 클래스의 메타 정보가 담긴
+		//					Class 객체의 주소를 반환하는 메소드
+		Class claszz = car.getClass();
+		// 만들어져 있는 class Car {} 의 메타 정보가 저장된 Class 객체 얻기
+		
+		// class Class 에 만들어져 있는 getPackageName() 메소드
+		// ---> class Car {} 의 메타 정보 중 "패키지 이름" 을 읽어 반환 해줍니다.
+		System.out.println("Car 클래스가 만들어져 있는 패키지 경로(메타 정보): " + claszz.getPackageName());
+		// "Car 클래스가 만들어져 있는 패키지 경로(메타 정보): 리플렉션"
+		
+		// class Class 에 만들어져 있는 getSimpleName() 메소드
+		// ---> class Car {} 의 메타 정보 중 패키지 이름을 제외한 "클래스 명" 만 읽어 반환 해줍니다.
+		System.out.println("new Car(); 객체를 만들 때 사용된 클래스 명(메타 정보): " + claszz.getSimpleName());
+		// "new Car(); 객체를 만들 때 사용된 클래스 명(메타 정보): Car"
+		
+		// class Class 에 만들어져 있는 getName() 메소드
+		// ---> class Car {} 의 메타 정보 중 "패키지명.클래스명" 을 읽어 반환 해줍니다.
+		System.out.println("Car 클래스가 만들어져 있는 패키지 경로를 포함한 클래스 전체 경로(메타 정보): " + claszz.getName());
+		// "Car 클래스가 만들어져 있는 패키지 경로를 포함한 클래스 전체 경로(메타 정보): 리플렉션.Car"
+		// (getPackageName() + "." + "getSimpleName() 와 같은 결과)
 		
 	}// === main 메소드
 
---
+++ 24자바_기본모듈_클래스들/src/리플렉션/ReflectionPrivaateFieldExample.java
@@ -0,0 +1,65 @@
+package 리플렉션;
+
+// Field 클래스
+// - 리플렉션을 통해 특정 클래스의 변수 정보를 다루기 위한 클래스
+import java.lang.reflect.Field;
+
+class Member {// 리플렉션 기법으로 정보를 조사할 특정 클래스
+	
+	private String name; // 회원 이름
+	private int age; // 회원 나이
+	
+	public Member() {} // 기본 생성자
+}
+
+public class ReflectionPrivaateFieldExample {
+
+	public static void main(String[] args) throws Exception {
+		
+		//1. 리플렉션 기법으로 정보를 조사할 Member 클래스의 객체 생성
+		Member member = new Member();
+		// new Member(); <- 기본 생성자를 호출해 객체 생성하면 String name = null, int age = 0; 으로 저장될 것임
+		
+		//2. class Class 의 new Class() 객체 얻기
+		// Member.class 문법을 사용하여 Member 클래스의 설계도 정보를 담고 있는 Class 객체 획득
+		// 이 Class 객체 내부에 모든 변수, 생성자, 메소드 정보(메타 정보)들이 저장되어 있습니다.
+		Class claszz = Member.class;
+		
+		//3. Member 클레스에 만들어 놓은 변수 정보 얻기
+		Field nameField =claszz.getDeclaredField("name");
+		// Class 라는 객체 통해서 "name" 라는 이름을 가진 변수 정보 검색
+		// private 로 선언된 name 변수도 포함하여 검색됩니다.
+		// 만약 name 변수를 찾지 못하면 NoSuchFieldException 예외가 발생됩니다.
+		
+		//4. private 접근 제한 해제
+		nameField.setAccessible(true);
+		// JVM 에게 접근 제어 검사를 하지 않도록 요청
+		// private -> 접근 가능 상태로 변경
+		// 없으면 IllegalAccessException
+		
+		//5. name 변수 값 강제로 설정
+		nameField.set(member, "홍길동");
+		// setter 메소드 호출 없이 강제로 "홍길동" 문자열 저장 가능
+		
+		//6. Member 클래스에 age 변수 정보 얻기
+		Field ageField = claszz.getDeclaredField("age");
+		
+		//7. private 접근 제한 해제
+		ageField.setAccessible(true);
+
+		//8. age 변수 값 강제로 설정
+		ageField.set(member, 30);
+		// member 객체의 age 인스턴스 변수에 정수 값 30을 강제로 저장
+		
+		//9. 결과 확인(리플렉션으로 다시 읽어오기)
+		System.out.println("이름: " + nameField.get(member));
+		// 이름: 홍길동
+		
+		System.out.println("나이: " + ageField.get(member));
+		// 나이: 30
+		
+	}// === main 메소드
+
+}// --- ReflectionPrivaateFieldExample 클래스
+
+
