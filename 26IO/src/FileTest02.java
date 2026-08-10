cf265cd5363a25c76c81161b0061a4aab0ffba1b
yuseungmin <dbtmdals7213@gmail.com>
2026-08-10 14:52:38 +0900

    2026 08 10 5교시

--- 26IO/src/FileTest02.java
+++ 26IO/src/FileTest02.java
@@ -1,9 +1,10 @@
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileTest02 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		// 현재 26IO 디렉터리 정보를 얻기 위한 File 클래스의 객체 메모리 생성
 		File fileDir = new File(".");
@@ -14,8 +15,72 @@ public class FileTest02 {
 		for(int i = 0; i < strs.length; i++) {
 			
 			System.out.println(strs[i]);
+		}// for 반복문
+		
+		// d.txt 파일에 접근해서 정보를 보기 위해 File 클래스의 객체 메모리 생성
+		File file = new File("C:\\a\\d.txt");
+		
+		/*
+			File 클래스의 delete() 메소드를 호출해서 사용하면 d.txt 파일 삭제 후 
+			삭제에 성공하면 true 반환, 삭제에 실패하면 false 반환하게 됩니다.
+		*/
+		if(file.delete()) {// d.txt 파일 삭제 후 삭제에 성공했느냐? 라고 조건식
+			
+			System.out.println("d.txt 파일 삭제 완료");
+		}else {// d.txt 파일 삭제에 실패했느냐?
+			
+			System.out.println("d.txt 파일 삭제 실패");
 		}
 		
+		System.out.println("---------------------------");
+		
+		// 실제 만들어져 있지 않은 b 디렉터리를 a 디렉터리 내부에 새로 만들어 접근하기 위해 File 클래스의 객체 메모리 생성!
+		// 작성 방법: 새로 만들 b 디렉터리의 경로를 생성자로 전달해서 File 클래스의 객체 생성
+		File file2 = new File("C:\\a\\b");
+		
+		file2.mkdir(); // <--- 디렉터리 생성
+		// file2.delete(); // <--- 디렉터리 또는 파일 색제
+		
+		/*
+			File 클래스에서 제공해주는 isDirectory() 메소드는
+			File 클래스의 객체 생성 시 전달한 전체 경로의 주소가 실제 디렉터리이냐? 라고 물어보는 메소드로
+			디렉터리이면? true 반환하고, 디렉터리가 아니면? false 반환하는 메소드입니다.
+		*/
+		if(file2.isDirectory()) {
+			
+			System.out.println("b 는 디렉터리입니다."); // <--- 출력
+		}else {
+			
+			System.out.println("b 는 파일입니다.");
+		}
+		
+		file2 = new File("C:\\a\\c");
+		
+		file2.createNewFile(); // c 파일 새로 생성 후 생성에 성공하면 true 반환, 실패하면 false 반환
+		
+		/*
+			File 클래스에서 제공해주는 isFile() 메소드는
+			File 클래스의 객체 생성 시 생성자로 전달한 전체 경로의 주소가 실제 파일이냐? 라고 물어보는 메소드로
+			실제 파일이면? true 반환하고, 파일이 아니면? false 를 반환하는 메소드입니다.
+		*/
+		if(file2.isFile()) {
+			
+			System.out.println("c 는 파일입니다."); // <--- 출력
+		}else {
+			
+			System.out.println("c 는 디렉터리입니다.");
+		}
+		
+		System.out.println("---------------------------");
+		
+		File file3 = new File("C:\\c");
+		
+		// File 클래스에서 제공해주는 mkdirs() 메소드를 호출하면
+		// File 클래스의 생성자로 전달한 디렉터리가 존재하지 않으면? c 디렉터리를 생성합니다.
+		file3.mkdirs();
+		
+		File file4 = new File("C:\\x\\y\\z");
+		file4.mkdirs();
 		
 	}// === main Method
 
---
+++ 26IO/src/IOTest03.java
@@ -0,0 +1,46 @@
+
+// 주제: C:\Users\KHBS_D_04\Desktop\workspace_java\25컬렉션그리고제네릭\src\Collections01.java 파일에 기록된 데이터들을 모두 읽어와 모니터에 출력
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class IOTest03 {
+
+	public static void main(String[] args) {
+		
+		//1. Collections01.java 파일에 기록된 전체 데이터 중에서
+		//	 1byte(8bit) 메모리에 저장할 크기의 데이터만 읽어와 저장할 변수
+		int data = 0;
+		
+		//2. 읽어들일 데이터가 기록된 Collections01.java 파일의 전체 경로(절대 경로)를 문자열로 만들어 변수에 저장
+		String path = "C:\\Users\\KHBS_D_04\\Desktop\\workspace_java\\25컬렉션그리고제네릭\\src\\Collections01.java";
+		
+		try {
+			//3. Collections01.java 파일에 기록된 바이트 단위로 읽어들이기 위한 입력 스트림 통로 역할의 FileInputStream 클래스의 객체 생성
+			// 작성 방법1. FileInputStream 참조변수 = new FileInputStream("파일절대경로");
+			FileInputStream fis = new FileInputStream(path);
+		
+			// 작성 방법2.	 File file = new File("파일절대경로");
+			//			 FileInputStream 참조변수 = new FileInputStream(file 객체 전달);
+			
+			//4. Collections01.java 파일에 기록된 전체 데이터 중에서 1바이트 단위로 반복해서 읽어들여 모니터 화면에 내보내어 출력
+			while((data = fis.read()) != -1) {
+				
+				// read() 메소드를 호출해 new FileInputStream(path); 입력 스트림 통로에서 한 번 1바이트 읽어들인 내용을
+				// System.out -> PrintStream 출력 스트림 통로 객체를 통해서 모니터 화면에 1바이트씩 내보내어 반복해서 출력
+				System.out.write((char)data);
+			}
+			
+		}catch(Exception e) {// <--- new FileNotFoundException 또는 IOException 자식 객체의 주소를 업 캐스팅해서 모두 전달 받음.
+			
+			e.printStackTrace(); // 결론: 어떤 예외가 발생하던지 간에 모든 예외 메세지 출력 가능, 모든 예외 처리 코드 작성 가능
+		}
+		
+		
+		
+	}// === main Method
+
+}// --- IOTest03 Class
+
+
